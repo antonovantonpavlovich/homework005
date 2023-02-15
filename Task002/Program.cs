1d16b35a69402ac51bb8350575c2ffd475ede0c8
Anton <9264622898@mail.ru>
2023-02-16 00:21:32 +0300

    lesson005

---
+++ Task002/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] InitArray()
+{
+int[] result = new int[4];
+Random rnd = new Random();
+
+for (int i = 0; i < result.Length; i++)
+{
+result[i] = rnd.Next(-100, 101);
+}
+
+return result;
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+}
+
+int GetPositiveSum(int[] array)
+{
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (i%2!=0)
+{
+    sum = sum + array[i];
+}
+
+}
+
+return sum;
+}
+
+int[] arr = InitArray();
+PrintArray(arr);
+int sum = GetPositiveSum(arr);
+
+Console.WriteLine($"-> {sum}");
+
---
+++ Task003/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+int[] InitArray()
+{
+int[] result = new int[4];
+Random rnd = new Random();
+
+for (int i = 0; i < result.Length; i++)
+{
+result[i] = rnd.Next(0, 101);
+}
+
+return result;
+}
+
+void PrintArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+}
+
+int GetPositiveSum(int[] array)
+{
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (i%2!=0)
+{
+    sum = sum + array[i];
+}
+
+}
+
+return sum;
+}
+
+int[] arr = InitArray();
+PrintArray(arr);
+int sum = GetPositiveSum(arr);
+
+Console.WriteLine($"-> {sum}");
+
+
